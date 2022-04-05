708d88d61cf5f5ac4ca75e5b4792797620cec7af
Deepika singh <97024366+deepika24597@users.noreply.github.com>
2022-04-05 13:44:31 +0530

    Update numbervalidation.js

--- numbervalidation.js
+++ numbervalidation.js
@@ -1,5 +1,6 @@
-function telephoneCheck(str) {
-    // Good luck!
+function telephoneCheck(str)
+{
+   
     let regex=/1{0,1}\s{0,1}((\(\d{3}\))|(\d{3}))[-]{0,1}[ ]{0,1}\d{3}[- ]{0,1}\d{4}/;
      console.log(str.match(regex));
    
